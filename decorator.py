b74a8fe6fb39dd89f87daad17f973a7a925b98e5
sieg <1357852347@qq.com>
2018-12-29 18:11:10 +0800

    Create decorator.py

---
+++ decorator.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import time, functools
+
+
+def metric(func):
+    @functools.wraps(func)
+    def warpper(*args, **kw):
+        begin=time.time()
+        func(*args,**kw)
+        end=time.time()
+        print('%s executed in %s ms' % (func.__name__,end-begin))
+        return func(*args, **kw)
+    return warpper
+
+
+# 测试
+@metric
+def fast(x, y):
+    time.sleep(0.0012)
+    return x + y
+
+
+@metric
+def slow(x, y, z):
+    time.sleep(0.1234)
+    return x * y * z
+
+
+f = fast(11, 22)
+s = slow(11, 22, 33)
+if f != 33:
+    print('测试失败!')
+elif s != 7986:
+    print('测试失败!')
