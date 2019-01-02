d4f384aa775816317f8b7dbd563adb233d9d2eac
sieg <1357852347@qq.com>
2019-01-02 10:04:09 +0800

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,17 +1,48 @@
 # *-* coding: utf-8 *-*
+import sys
+
+sys.path.append('List')
+from SinglyLinkedList import SinglyLinkedList
+
+
+def reversed(node):
+    reverse_head = None
+    while node:
+        next = node.next
+        node.next = reverse_head
+        reverse_head = node
+        node = next
+    return reverse_head
+
 
 def is_palindrome(list):
     fast = list._head
     slow = list._head
+    position = 0
     while fast and fast.next:
         fast = fast.next.next
         slow = slow.next
+        position += 1
+    reverse_head = reversed(slow)
+    is_plain = True
+    node = list._head
+    while node and reverse_head:
+        if node.data == reverse_head.data:
+            node = node.next
+            reverse_head = reverse_head.next
+        else:
+            is_plain = False
+            break
+    return is_plain
 
 
-def reversed(node):
-    tmp = None
-    while node:
-        next = node.next
-        node.next = tmp
-        tmp = node
-        node = next
+if __name__ == '__main__':
+    s = SinglyLinkedList()
+    s.add_node(1)
+    s.add_node(2)
+    s.add_node(3)
+    s.add_node(3)
+    s.add_node(2)
+    s.add_node(1)
+    a = is_palindrome(s)
+    print(a)
